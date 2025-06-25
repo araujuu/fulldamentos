3c466a3eb58d1e41e754ee101c3ef1c041cd1873
julia <contatojuliaaraujo64@gmail.com>
2025-06-25 14:07:46 -0300

    feat: adicionando DOM e exercicio

---
+++ javascript/exercicio_01/index.js
@@ -0,0 +1,22 @@
+console.log("Bem vindos ao consultorio do Dr Potato!")
+
+console.log("Digite suas opções: ")
+
+console.log("1- Ver lista de pacientes")
+console.log("2- Novo paciente")
+console.log("3- Consultar paciente")
+console.log("1- Sair")
+
+console.log("Escolha uma opção: ")
+
+if(option == 1){
+
+}
+
+function Pacientes(){
+    pacientes = ["Julia", "Matheus", "Marcos"]
+}
+
+console.log("Lista de pacientes: ", pacientes)
+
+console.log("Novo paciente")
